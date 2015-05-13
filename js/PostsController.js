059cdf3fcca04d91beabf07ae497bed77a5f66e1
Scott M Miller <smmiller.work@gmail.com>
2015-05-13 11:19:43 -0600

    steps 2 & 3 completed

--- js/PostsController.js
+++ js/PostsController.js
@@ -2,6 +2,13 @@ var app = angular.module('reddit');
 
 app.controller('PostsController', function($scope, firebaseService) {
 
-
+	var getPosts = function() {
+		firebaseService.getData()
+			.then(function(data) {
+		$scope.posts = data;
+		console.log('data', data);
+		});
+	}
+	getPosts();
 
 });
--- js/firebaseService.js
+++ js/firebaseService.js
@@ -1,7 +1,16 @@
 var app = angular.module('reddit');
 
-app.service('firebaseService', function() {
+app.service('firebaseService', function($http, $q) {
 
-	
+	this.getData = function() {
+		return $http({
+			method: 'GET',
+			url: 'https://devmtn.firebaseio.com/posts.json',
+			
+		}).then(function(data) {
+			return data.data;
+		});
+	};
+
 
 });

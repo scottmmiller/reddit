0c0fd143b006838b452ddb187635c052c725586b
Scott M Miller <smmiller.work@gmail.com>
2015-05-13 10:33:54 -0600

    step 1 complete; added bower_components v cdn's;

---
+++ js/PostsController.js
@@ -0,0 +1,7 @@
+var app = angular.module('reddit');
+
+app.controller('PostsController', function($scope, firebaseService) {
+
+
+
+});
---
+++ js/firebaseService.js
@@ -0,0 +1,7 @@
+var app = angular.module('reddit');
+
+app.service('firebaseService', function() {
+
+	
+
+});

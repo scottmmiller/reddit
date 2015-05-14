50d30ff5e1cd1c868401aab37677fb32f3db76da
Scott M Miller <smmiller.work@gmail.com>
2015-05-13 19:50:35 -0600

    completed thru BlackDiamond - 'Comments'

--- js/PostsController.js
+++ js/PostsController.js
@@ -2,13 +2,43 @@ var app = angular.module('reddit');
 
 app.controller('PostsController', function($scope, firebaseService) {
 
+	$scope.addPost = function() {
+		$scope.newPost.timestamp = Date.now();
+		$scope.newPost.comments = [];
+		$scope.newPost.karma = 0;
+		$scope.newPost.id = guid();
+		firebaseService.addPost($scope.newPost)
+			.then(function(data) {
+				getPosts();
+			});		
+	};
+
+	$scope.vote = function(id, direction) {
+		console.log('scope.vote posts: ', $scope.posts[id]);
+		firebaseService.vote(id, direction, $scope.posts[id].karma)
+			.then(function(data) {
+				getPosts();
+			});
+	};
+
 	var getPosts = function() {
 		firebaseService.getData()
 			.then(function(data) {
 		$scope.posts = data;
-		console.log('data', data);
+		console.log(' Ctrlr getPosts data: ', data);
 		});
 	}
 	getPosts();
+
+	var guid = function() {
+	    var s4 = function() {
+	      return Math.floor((1 + Math.random()) * 0x10000)
+	        .toString(16)
+	        .substring(1);
+	    };
+	    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+	      s4() + '-' + s4() + s4() + s4();
+	};
+
 
 });
--- js/firebaseService.js
+++ js/firebaseService.js
@@ -3,14 +3,52 @@ var app = angular.module('reddit');
 app.service('firebaseService', function($http, $q) {
 
 	this.getData = function() {
-		return $http({
+		var deferred = $q.defer();
+		$http({
 			method: 'GET',
-			url: 'https://devmtn.firebaseio.com/posts.json',
+			url: 'https://devmtn.firebaseio.com/posts.json'
 			
 		}).then(function(data) {
-			return data.data;
+			deferred.resolve(data.data);
+		}, function(error) {
+			deferred.reject(error);
+		});
+		return deferred.promise;
+	};
+
+	this.addPost = function(post) {
+		var deferred = $q.defer();
+		console.log(post);
+		return $http({
+			method: 'PUT',
+			url: 'https://devmtn.firebaseio.com/posts/' + post.id + '.json',
+			data: post
+		}).then(function(data) {
+			console.log('post res', data);
 		});
 	};
 
+	this.vote = function(postId, direction, karma) {
+		var deferred = $q.defer();
+
+		if(direction === 'up') {
+			karma++;
+		}
+		else if (direction === 'down') {
+			karma--;
+		}
+
+		$http({
+			method: 'PATCH',
+			url: 'https://devmtn.firebaseio.com/posts/' + postId + '.json',
+			data: {karma: karma}
+		}).then(function(data) {
+			console.log('karma: ', data);
+			deferred.resolve(data);
+		}, function(error) {
+			deferred.reject(error);
+		});
+		return deferred.promise;
+	};
 
 });
